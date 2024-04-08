b15185238b3afb29c17507ddd8e1c5ec17dfe5b7
taplongevity <sagotiyan@vmware.com>
2024-04-08 01:00:22 -0700

    1712563221

--- where-for-dinner-ui/src/DiningSearches.js
+++ where-for-dinner-ui/src/DiningSearches.js
@@ -16,7 +16,7 @@ function DiningSearches(props)
         <div name="diningSearches" id="diningSearches">
           <div className="bannerHeader"> 
             <div className="bannerCenter">
-            <label className="searchesHeader">Submitted Searches And Results1712556021</label>
+            <label className="searchesHeader">Submitted Searches And Results1712563221</label>
             </div>      	
           </div>           
           {searches}
